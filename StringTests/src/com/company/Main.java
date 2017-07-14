1655e1156508b2c4dd16e30cdbdc3090044029c8
Alex Redchets <alexey.redchets@gmail.com>
2017-07-14 06:24:43 -0400

    Working on isBalanced() method

--- StringTests/src/com/company/Main.java
+++ StringTests/src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
 import java.util.Objects;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
         String str = "heleh";
-        System.out.println(isPalendrom(str));
+        //System.out.println(isPalendrom(str));
+        System.out.println(isBalanced("s{}dgsg"));
     }
 
     public static String reverseString(String str){
@@ -21,5 +23,35 @@ public class Main {
     public static boolean isPalendrom(String str){
         String temp = reverseString(str);
         return Objects.equals(temp, str);
+    }
+
+    public static boolean isBalanced(String str){
+        char[] arr = str.toCharArray();
+        System.out.println(arr[2]);
+        Stack<Character> stack = new Stack<>();
+        for (char anArr : arr) {
+            switch (anArr){
+                case '(':
+                case '[':
+                case '{':
+                    System.out.println("Got it");
+                    stack.push(anArr);
+                case ')':
+                    if (stack.empty() || stack.pop() != ')'){
+                        return false;
+                    }
+                case ']':
+                    if (stack.empty() || stack.pop() != ']'){
+                        return false;
+                    }
+                case '}':
+                    if (stack.empty() || stack.pop() != '}'){
+                        return false;
+                    }
+                    System.out.println("Got it");
+            }
+            return !stack.empty();
+        }
+        return true;
     }
 }
