75782a6185476957e3a0f021c15e93d304d574ec
Alex Redchets <alexey.redchets@gmail.com>
2017-07-17 17:51:16 -0400

    Recreating Set data structure

--- Data Structutes/src/Main.java
+++ Data Structutes/src/Main.java
@@ -1,21 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        Set<Integer> set = new Set<>();
-        set.add(1);
-        set.add(2);
-        set.add(3);
+        ArraySet<Integer> set = new ArraySet<>();
         set.add(4);
-        set.add(5);
-        set.show();
-        System.out.println(set.size());
-        set.add(5);
-        set.add(1);
-        set.add(5);
         set.add(3);
+        set.add(7);
         set.add(5);
         set.show();
-        System.out.println(set.size());
     }
 }
 
--- Data Structutes/src/Set.java
+++ Data Structutes/src/Set.java
@@ -1,65 +1,32 @@
 public class Set<T> {
 
-    Node head;
-    int listCount;
+    private T collection[];
+    private int setCount;
 
     public Set() {
-        listCount = 0;
+        collection = null;
     }
 
-    public boolean has(T value){
-        if (head == null){
-            System.out.println("Set is empty");
-        }
-        else {
-            Node current = head;
-            while (current.next != null){
-                if (current.value == value){
+    public Set(T[] collection) {
+        this.collection = collection;
+        setCount = collection.length;
+    }
+
+    public boolean contains(T item){
+        if (item == null){
+            for (int i = 0; i < setCount; i++){
+                if (collection[i] == null){
                     return true;
                 }
-                current = current.next;
             }
-            return current.value == value;
+        }
+        else {
+            for (T anArr : collection){
+                if (anArr == item){
+                    return true;
+                }
+            }
         }
         return false;
-    }
-
-    public void add(T value){
-        Node newNode = new Node(value);
-        if (head == null){
-            head = newNode;
-        }
-        else {
-            Node current = head;
-            while (current.next != null){
-                if (current.value == value){
-                    return;
-                }
-                current = current.next;
-            }
-            if (current.value == value){
-                return;
-            }
-            current.next = newNode;
-        }
-        listCount ++;
-    }
-
-    public void show(){
-        if (head == null){
-            System.out.println("Set is empty");
-        }
-        else {
-            Node current = head;
-            System.out.print("[");
-            while (current.next != null){
-                System.out.print(current.value + ", ");
-                current = current.next;
-            }
-            System.out.print(current.value + "]");
-        }
-    }
-    public int size(){
-        return listCount;
     }
 }
